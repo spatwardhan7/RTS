ee5ddcf06e86e96fab8b8f3703e926c00c5d5e4e
Saurabh Patwardhan <saurabh.patwardhan89@gmail.com>
2013-11-09 16:18:33 -0500

    Removing services.php from repo

--- DM/services.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-		$jemin = json_decode(file_get_contents
-	                ("http://bruiser3.cc.gatech.edu:8080/grait-dm/feed_usgs_m25_week.php"),
-	                TRUE);
-		echo json_encode($jemin);
-		return $jemin;
-
-?>
