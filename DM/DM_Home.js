b2379662b5e20ef96b97ab64dc6e986f1af858b1
Saurabh Patwardhan <saurabh.patwardhan89@gmail.com>
2013-11-09 18:46:15 -0500

    Dropdown date picker --- start date initilization pending

--- DM/DM_Home.js
+++ DM/DM_Home.js
@@ -55,6 +55,7 @@ function initialize_date()
 
 function displayDisasterType()
 {
+	populatedropdown("daydropdown","monthdropdown","yeardropdown","daydropdown_start","monthdropdown_start","yeardropdown_start");
 	$('#selectButtons').hide();
 	$('#selectDisasters').show();
 	initialize_date();
@@ -134,6 +135,71 @@ function displayDisasterMap()
 	    $('#backButton').show();
 		}
 }
+
+var monthtext=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sept','Oct','Nov','Dec'];
+
+function populatedropdown(dayfield, monthfield, yearfield, dayfield_start, monthfield_start, yearfield_start)
+{
+	var today=new Date();
+	
+	
+	var dayfield=document.getElementById(dayfield);
+	var monthfield=document.getElementById(monthfield);
+	var yearfield=document.getElementById(yearfield);
+	
+	console.log(dayfield);
+	console.log(monthfield);
+	console.log(yearfield);
+	
+	for (var i=0; i<31; i++)
+		dayfield.options[i]=new Option(i, i+1);
+
+	dayfield.options[today.getDate()]=new Option(today.getDate(), today.getDate(), true, true); //select today's day
+	
+	for (var m=0; m<12; m++)
+		monthfield.options[m]=new Option(monthtext[m], monthtext[m]);
+	
+	monthfield.options[today.getMonth()]=new Option(monthtext[today.getMonth()], monthtext[today.getMonth()], true, true); //select today's month
+
+	var thisyear=today.getFullYear();
+	for (var y=0; y<20; y++)
+	{
+		yearfield.options[y]=new Option(thisyear, thisyear);
+		thisyear+=1;
+	}
+	yearfield.options[0]=new Option(today.getFullYear(), today.getFullYear(), true, true); //select today's year
+
+
+/*-----Start Date Populate-----*/
+
+	var dayfield_start = document.getElementById(dayfield_start);
+	var monthfield_start=document.getElementById(monthfield_start);
+	var yearfield_start=document.getElementById(yearfield_start);
+
+	console.log(dayfield_start);
+	console.log(monthfield_start);
+	console.log(yearfield_start);
+
+
+	for (var i=0; i<31; i++)
+		dayfield_start.options[i]=new Option(i, i+1);
+
+	dayfield_start.options[today.getDate()]=new Option(today.getDate(), today.getDate(), true, true); //select today's day
+	
+	for (var m=0; m<12; m++)
+		monthfield_start.options[m]=new Option(monthtext[m], monthtext[m]);
+	
+	monthfield_start.options[today.getMonth()]=new Option(monthtext[today.getMonth()], monthtext[today.getMonth()], true, true); //select today's month
+
+	var thisyear=today.getFullYear();
+	for (var y=0; y<20; y++)
+	{
+		yearfield_start.options[y]=new Option(thisyear, thisyear);
+		thisyear+=1;
+	}
+	yearfield_start.options[0]=new Option(today.getFullYear(), today.getFullYear(), true, true); //select today's year
+}
+
 
 function startMap(data,map,checkbox_type)
 {	
